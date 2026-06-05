b3e82210bdbbdc5ef6fb37f866a7827d1733ab1d
saniyababar424-cloud <saniyababar424@gmail.com>
2026-06-05 09:25:38 +0530

    Boolean Surprise

---
+++ BooleanSurprise.java
@@ -0,0 +1,7 @@
+public class BooleanSurprise {
+    public static void main(String[] args){
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(true + " Java");
+    }
+}
