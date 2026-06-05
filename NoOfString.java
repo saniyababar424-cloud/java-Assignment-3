1b2a4dd2c58fa8c7a29f6972079f381c8de23f0a
saniyababar424-cloud <saniyababar424@gmail.com>
2026-06-05 09:09:35 +0530

    No of String

---
+++ NoOfString.java
@@ -0,0 +1,7 @@
+public class NoOfString {
+    public static void main(String args[]){
+        System.out.println(10 + 20);
+        System.out.println("10" + "20");
+        System.out.println("10" + 20);
+    }
+}
