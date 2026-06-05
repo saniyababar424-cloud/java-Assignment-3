92459617c4d6362efa769280835e860982da7d14
saniyababar424-cloud <saniyababar424@gmail.com>
2026-06-05 09:06:32 +0530

    space confusion

---
+++ SpaceConfusion.java
@@ -0,0 +1,6 @@
+public class SpaceConfusion {
+    public static void main(String args[]) {
+        System.out.println("Java" + "Programming");
+        System.out.println("Java" + " " + "Programming");
+    }
+}
